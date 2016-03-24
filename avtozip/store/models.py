d08276905d57967d75da0457b4b80769d2ee0325
zitoss <ashishov1990@gmail.com>
2016-03-25 01:40:06 +0300

    Issue 1 - Create product model with known fields Create model of Product with fields:  - article  - name  - cost  - price  - count
    Fixes #1

--- avtozip/store/models.py
+++ avtozip/store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    article = models.CharField(max_length=50)
+    name = models.CharField(max_length=200)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    count = models.DecimalField(max_digits=10, decimal_places=2)
+

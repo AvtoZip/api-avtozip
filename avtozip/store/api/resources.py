75db97d024a79f631821aac3832ad75d02dc95ce
zitoss <ashishov1990@gmail.com>
2016-03-28 03:08:19 +0300

    Issue 20 - Enhance product filtering by article/name icontains
    - remove `icontains` filter for field `name`
    - append multi-`icontains` filter by fields `article` and `name`
    - need additional investigation and reimplementation of the solution in the future
    
    You can see the result by url:
    http://localhost:8012/api/v1/product/?format=json&query=<your_query>
    
    Fixes #20

--- avtozip/store/api/resources.py
+++ avtozip/store/api/resources.py
@@ -1,5 +1,7 @@
 """Store API resources."""
 
+from django.db.models import Q
+
 from tastypie import fields
 from tastypie.http import HttpForbidden
 from tastypie.resources import ModelResource
@@ -34,9 +36,36 @@ class ProductResource(StoreBaseResource):
 
     category = fields.ForeignKey(ProductCategoryResource, 'category')
 
+    QUERY_KEY = 'query'
+
     class Meta:
         queryset = Product.objects.all()
-        allowed_methods = ['post', 'put', 'get', 'patch', 'delete']
-        filtering = {
-            'name': ('exact',),
-        }
+        allowed_methods = ('post', 'put', 'get', 'patch', 'delete')
+        # TODO need to implement correct handling of custom_filters
+        filtering = {}
+        # TODO END
+
+    def build_filters(self, filters=None, ignore_bad_filters=False):
+        """Build required filters. Expanded by filter `query`."""
+        if filters is None:
+            filters = {}
+
+        orm_filters = super(ProductResource, self).build_filters(filters, ignore_bad_filters)
+
+        # Append orm_filter `query`
+        if self.QUERY_KEY in filters:
+            query = filters[self.QUERY_KEY]
+            qset = Q(article__icontains=query) | Q(name__icontains=query)
+            orm_filters.update({self.QUERY_KEY: qset})
+
+        return orm_filters
+
+    def apply_filters(self, request, applicable_filters):
+        """Apply applicable filters. Expanded by filter `query`."""
+        if self.QUERY_KEY in applicable_filters:
+            custom = applicable_filters.pop(self.QUERY_KEY)
+        else:
+            custom = None
+
+        semi_filtered = super(ProductResource, self).apply_filters(request, applicable_filters)
+        return semi_filtered.filter(custom) if custom else semi_filtered
